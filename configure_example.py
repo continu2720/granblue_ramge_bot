04f005976c3e4eb4e39dfd3935845372c1b9899f
Ubuntu <ubuntu@ip-172-26-5-199.ap-northeast-2.compute.internal>
2020-09-14 06:23:41 +0000

    configure 예시 추가

---
+++ configure_example.py
@@ -0,0 +1,13 @@
+# https://developer.twitter.com/en/apply-for-access
+consumer_key = "1234"
+consumer_secret = "1234"
+
+access_token = "1234"
+access_token_secret = "1234"
+
+# https://notify-bot.line.me/en/
+line_token = "1234"
+
+# https://developers.naver.com/apps/#/register
+papago_client_id = "1234"
+papago_secret = "1234"
